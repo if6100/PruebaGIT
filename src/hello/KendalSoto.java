6dd963bed31edac8667e994dfa3ccf87e1b40bc1
Kendal <88624733+KendSoto02@users.noreply.github.com>
2022-11-02 17:57:46 -0600

    Update KendalSoto.java

--- src/hello/KendalSoto.java
+++ src/hello/KendalSoto.java
@@ -1,7 +1,10 @@
-pakage hello;
+package hello;
 
-public class KendalSoto{
-  
-  
-  
+public class KendalSoto extends MensajeCadena{
+
+  public String agregarMensaje(String mensaje){
+    mensaje+= "Kendal says:FC Barcelona";
+    //System.out.println("mensaje profesor");
+    return siguienteCadena(mensaje);
+  }
 }
