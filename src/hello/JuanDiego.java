d8d06a3aada536c11c0f30d2ce326e338bfaf786
Juan-Diego07 <54189967+Juan-Diego07@users.noreply.github.com>
2022-11-02 17:57:42 -0600

    Update JuanDiego.java

--- src/hello/JuanDiego.java
+++ src/hello/JuanDiego.java
@@ -1,11 +1,11 @@
 package hello;
 
-public class JuanDiego{
+public class JuanDiego extends MensajeCadena{
 
-
-
-
-
-
-
+  public String agregarMensaje(String mensaje){
+    mensaje+= "Profesor:hola";
+    System.out.println("mensaje profesor");
+    return siguienteCadena(mensaje);
+  }
 }
+
