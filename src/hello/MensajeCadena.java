d810c4edd44cbf7fda75627897e2aa984cdc2bcd
if6100 <116769034+if6100@users.noreply.github.com>
2022-11-02 11:10:02 -0600

    Update MensajeCadena.java

--- src/hello/MensajeCadena.java
+++ src/hello/MensajeCadena.java
@@ -3,6 +3,15 @@ package hello;
 public abstract class MensajeCadena{
   private MensajeCadena siguiente;
    
+  public static MensajeCadena link(MensajeCadena first, MensajeCadena... chain) {
+        MensajeCadena head = first;
+        for (MensajeCadena nextInChain: chain) {
+            head.next = nextInChain;
+            head = nextInChain;
+        }
+        return first;
+   }
+  
   public abstract String agregarMensaje(String mensaje);
    
   protected String siguienteCadena(String mensaje) {
