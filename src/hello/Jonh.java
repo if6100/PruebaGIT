a5d9c93130b91228d3851e0ca29e799c25a1a842
Jonathan <117307594+KODRUCK@users.noreply.github.com>
2022-11-02 17:56:32 -0600

    Update Jonh.java

--- src/hello/Jonh.java
+++ src/hello/Jonh.java
@@ -1,1 +1,10 @@
-packet hello;
+package hello;
+
+public class Jonh extends MensajeCadena{
+
+  public String agregarMensaje(String mensaje){
+    mensaje+= "Profesor:hola";
+    System.out.println("Que tal esta todo profe?");
+    return siguienteCadena(mensaje);
+  }
+}
