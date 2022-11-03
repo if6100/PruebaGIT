7a1ea8a5f39dfd3cbd78590fad2b22091aef10ff
Jonathan <117307594+KODRUCK@users.noreply.github.com>
2022-11-02 18:28:30 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloWorld {
   public static void main(String[] args) {
 
-	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(),new ISB(), new BrihanArrietaCadena());
+	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(),new ISB(), new BrihanArrietaCadena(),new Jonh());
 	System.out.println(mensaje.agregarMensaje("prueba"));
   }
 }
