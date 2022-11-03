f459cb9db70c766be9dd77af248471ab972bbf0e
Baigv25 <102562653+Baigv25@users.noreply.github.com>
2022-11-02 18:21:15 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -2,7 +2,7 @@ package hello;
 
 public class HelloWorld {
   public static void main(String[] args) {
-	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),BaironCadena());
+	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(), new BaironCadena());
 	System.out.println(mensaje.agregarMensaje("prueba"));
   }
 }
