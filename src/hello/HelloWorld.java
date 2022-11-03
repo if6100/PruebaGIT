3309c3bd839d3aab282d5c733409b9df1088cf7e
if6100 <116769034+if6100@users.noreply.github.com>
2022-11-02 18:36:06 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
   public static void main(String[] args) {
 
 	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(), new BrihanArrietaCadena(),
-  new Jonh(), new BaironCadena(), new OrlandoA(),new JuanDiego(), new DanielCadena(), new JairoEBG(), new NicoleCadena());	
+  new Jonh(), new BaironCadena(), new OrlandoA(),new JuanDiego(), new DanielCadena(), new NicoleCadena());	
 
 	System.out.println(mensaje.agregarMensaje("prueba"));
   }
