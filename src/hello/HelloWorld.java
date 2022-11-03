8d323d26a7eb2a321de2e92f9c65489931b1d477
roibinec <42971724+roibinec@users.noreply.github.com>
2022-11-02 18:23:32 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -2,6 +2,13 @@ package hello;
 
 public class HelloWorld {
   public static void main(String[] args) {
+  
+  
+  
+  
+  
+  
+  
 	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(), new RoibinCadena);
 	System.out.println(mensaje.agregarMensaje("prueba"));
   }
