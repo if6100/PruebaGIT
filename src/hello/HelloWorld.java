f42fe4a250f0187ef1638fe81f30f41ab001527b
alearal12 <alejandro.araya@ucrso.info>
2022-11-02 18:23:03 -0600

    Second commit

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -2,7 +2,7 @@ package hello;
 
 public class HelloWorld {
   public static void main(String[] args) {
-	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(),new Jonh());
+	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(),new Jonh(), new OrlandoA());
 	System.out.println(mensaje.agregarMensaje("prueba"));
   }
 }
