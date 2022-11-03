b24929d4bb2b3aa1f62a656fcba8bb5d9d15e76b
Carlos Guevara <54116420+caguevara11@users.noreply.github.com>
2022-11-02 18:47:31 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
 	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(), new BrihanArrietaCadena(),	
 
-  new Jonh(), new BaironCadena(), new OrlandoA(),new JuanDiego(), new DanielCadena(), new NicoleCadena(), new KendalSoto(),new BlasAstrid());	
+  new Jonh(), new BaironCadena(), new OrlandoA(),new JuanDiego(), new DanielCadena(), new NicoleCadena(), new KendalSoto(),new BlasAstrid(), new CarlosGuevara());	
 
 	System.out.println(mensaje.agregarMensaje("prueba"));
   }
