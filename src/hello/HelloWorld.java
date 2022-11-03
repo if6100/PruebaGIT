fa729985dfa57dc74e882b487be00f91cfcd8a9b
Kendal <88624733+KendSoto02@users.noreply.github.com>
2022-11-02 18:30:41 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,5 @@ public class HelloWorld {
 	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(), new KendalSoto());
 	System.out.println(mensaje.agregarMensaje("prueba"));
 
-  MensajeCadena mensajeAlejandro=MensajeCadena.link(new AlejandroA());
-	System.out.println(mensajeAlejandro.agregarMensaje("prueba"));
   }
 }
