18872983e9115995a9fa26d52ba3c1a2503d9912
BlasG-Astrid <102562683+BlasG-Astrid@users.noreply.github.com>
2022-11-02 18:40:08 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
 	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(),new AlejandroA(),new Paulo(), new BrihanArrietaCadena(),
 
-  new Jonh(), new BaironCadena(), new OrlandoA(),new JuanDiego(), new DanielCadena(), new NicoleCadena());	
+  new Jonh(), new BaironCadena(), new OrlandoA(),new JuanDiego(), new DanielCadena(), new NicoleCadena(), new BlasAstrid());	
 
 
 	System.out.println(mensaje.agregarMensaje("prueba"));
