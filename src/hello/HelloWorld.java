3bc860b9a4411537b864bc94b0ea00b5b5995dc2
AllanNaranjo123 <102562961+AllanNaranjo123@users.noreply.github.com>
2022-11-02 18:22:40 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -2,10 +2,6 @@ package hello;
 
 public class HelloWorld {
   public static void main(String[] args) {
-	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena());
+	MensajeCadena mensaje=MensajeCadena.link(new ProfesorCadena(), new allan());
 	System.out.println(mensaje.agregarMensaje("prueba"));
-
-  MensajeCadena mensajeAlejandro=MensajeCadena.link(new AlejandroA());
-	System.out.println(mensajeAlejandro.agregarMensaje("prueba"));
-  }
 }
